d6381c855373929c36e7e0ee1ec636627ff33c22
smarqueslopez <smarqueslopez@gmail.com>
2021-09-27 23:48:53 +0200

    feat(global): :tada: Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>React Responsive Delivery Landing page</title>
+        <meta
+          name='description'
+          content='React Responsive Delivery Landing page'
+        />
+        <link rel='icon' href='./images/favicon.png' />
+      </Head>
+    </div>
+  )
+}
