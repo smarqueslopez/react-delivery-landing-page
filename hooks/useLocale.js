544882e3b7442ca6fc21c78987c635412f75c4ad
smarqueslopez <smarqueslopez@gmail.com>
2021-09-28 00:50:22 +0200

    feat(global): :globe_with_meridians: Adding i18n functionality

---
+++ hooks/useLocale.js
@@ -0,0 +1,6 @@
+import { useRouter } from 'next/router'
+
+export const useLocale = () => {
+  const { locales, locale } = useRouter()
+  return { locale, locales }
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-US', 'es-ES'],
+    defaultLocale: 'en-US',
+    localeDetection: false
+  }
 }
