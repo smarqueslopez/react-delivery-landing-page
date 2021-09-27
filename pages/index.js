feb844a827d62dcb171906930dfdd3e253e49b13
smarqueslopez <smarqueslopez@gmail.com>
2021-09-28 01:04:45 +0200

    feat(global): :lipstick: Adding boxicon stylesheet

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,10 @@ export default function Home() {
           content='React Responsive Delivery Landing page'
         />
         <link rel='icon' href='./images/favicon.png' />
+        <link
+          href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css'
+          rel='stylesheet'
+        />
       </Head>
     </div>
   )
