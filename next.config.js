72a6fd22240b5179eac2999259ffb8cf028ef6d9
smarqueslopez <smarqueslopez@gmail.com>
2021-09-28 12:14:07 +0200

    feat(global): :heavy_plus_sign: Adding loader svg dependency

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            titleProp: true
+          }
+        },
+        'file-loader'
+      ]
+    })
+    return config
+  },
   reactStrictMode: true,
   i18n: {
     locales: ['en-US', 'es-ES'],
