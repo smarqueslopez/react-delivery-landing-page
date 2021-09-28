03dc363e68070190eb67c616e3c9f21c614272d1
smarqueslopez <smarqueslopez@gmail.com>
2021-09-28 16:42:41 +0200

    feat(global): :sparkles: Adding contact section in main component

---
+++ components/Main/Contact/Contact.jsx
@@ -0,0 +1,80 @@
+import classNames from 'classnames'
+import { useTranslate } from '@/hooks/useTranslate'
+import layout from '@/styles/Layout.module.scss'
+import style from './Contact.module.scss'
+
+function Contact() {
+  const translate = useTranslate()
+
+  return (
+    <section
+      className={classNames(
+        [style.contact],
+        [layout.section],
+        [layout.container]
+      )}
+      id='contact'
+    >
+      <div className={classNames([style.contact__container], [layout.grid])}>
+        <div className={[style.contact__content]}>
+          <h2 className={[layout['section__title-center']]}>
+            {translate.main.contact.title}
+          </h2>
+          <p className={[style.contact__description]}>
+            {translate.main.contact.description}
+          </p>
+        </div>
+
+        <ul className={classNames([style.contact__content], [layout.grid])}>
+          <li className={[style.contact__address]}>
+            {`${translate.main.contact.info.phoneLabel}: `}
+            <a
+              href={`tel:${translate.main.contact.info.phone}`}
+              target='_blank'
+              rel='noreferrer'
+              title={translate.main.contact.info.phone}
+              className={[style.contact__information]}
+            >
+              {translate.main.contact.info.phone}
+            </a>
+          </li>
+          <li className={[style.contact__address]}>
+            {`${translate.main.contact.info.emailLabel}: `}
+            <a
+              href={`mailto:${translate.main.contact.info.email}`}
+              target='_blank'
+              rel='noreferrer'
+              title={translate.main.contact.info.phone}
+              className={[style.contact__information]}
+            >
+              {translate.main.contact.info.phone}
+            </a>
+          </li>
+          <li className={[style.contact__address]}>
+            {`${translate.main.contact.info.addressLabel}: `}
+            <a
+              href='https://maps.google.com'
+              target='_blank'
+              rel='noreferrer'
+              title={translate.main.contact.info.address}
+              className={[style.contact__information]}
+            >
+              {translate.main.contact.info.address}
+            </a>
+          </li>
+        </ul>
+        <div className={style.contact__content}>
+          <a
+            href='#'
+            className={layout.button}
+            title={translate.main.contact.contactUs}
+          >
+            {translate.main.contact.contactUs}
+          </a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -3,6 +3,7 @@ import About from './About/About'
 import Security from './Security/Security'
 import Services from './Services/Services'
 import InfoApp from './InfoApp/InfoApp'
+import ContactUs from './Contact/Contact'
 import style from './Main.module.scss'
 
 function Main() {
@@ -13,6 +14,7 @@ function Main() {
       <Security />
       <Services />
       <InfoApp />
+      <ContactUs />
     </main>
   )
 }
